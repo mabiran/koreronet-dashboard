219dc7a426952cce278d344dabb5bdfadf8805b1
M.Barzegar <42859920+mabiran@users.noreply.github.com>
2025-10-30 13:13:05 +1300

    Update koreronet-frontend.py

--- koreronet-frontend.py
+++ koreronet-frontend.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3##
 # -*- coding: utf-8 -*-
 # KōreroNET Dashboard (with splash + Drive) — daily auto-refresh + change-aware indexing
 # ------------------------------------------------------------
